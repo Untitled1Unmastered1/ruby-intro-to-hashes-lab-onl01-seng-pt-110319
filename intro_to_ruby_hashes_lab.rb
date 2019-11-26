5e8278c7bf05f519aed8877088351027bcf65e33
GABRIEL PEREZ <gabriel1994.gp@gmail.com>
2019-11-26 00:47:41 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,8 +15,8 @@ def monopoly
 	}
 end
 
-def monopoly_with_second_tier
-  monopoly = {railroads: {pieces: 4} {names: ={}
+def monopoly_with_second_tier{
+  monopoly = {railroads: {pieces: 4} {names: = {}
   }
 end
 
