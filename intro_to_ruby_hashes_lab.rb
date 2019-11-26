e00bca0acfc5b0d68b0039dcda9fd15cf851df48
GABRIEL PEREZ <gabriel1994.gp@gmail.com>
2019-11-26 00:58:03 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,7 +27,7 @@ end
 def monopoly_with_third_tier
    monopoly = {railroads: 
    {pieces: 4, 
-   
+   names: {reading_railroad: {}, pennsylvania_railroad: { , b_and_o_railroad: {}, shortline_railroad: {} },
 
 end
 
